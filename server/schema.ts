1878fd32ef7a7585cb788aa8f71b83553a723083
nima moradi <mnima8100@gmail.com>
2024-08-10 17:35:49 +0330

    created schema for users and accounts

--- server/schema.ts
+++ server/schema.ts
@@ -1,1 +1,40 @@
-import { pgTable, serial, text } from "drizzle-orm/pg-core";
+import { integer, pgTable, primaryKey, text, timestamp } from "drizzle-orm/pg-core";
+import type { AdapterAccount } from "next-auth/adapters"
+
+
+
+export const users = pgTable("user", {
+    id: text("id")
+      .primaryKey()
+      .$defaultFn(() => crypto.randomUUID()),
+    name: text("name"),
+    email: text("email").unique(),
+    emailVerified: timestamp("emailVerified", { mode: "date" }),
+    image: text("image"),
+  })
+   
+  export const accounts = pgTable(
+    "account",
+    {
+      userId: text("userId")
+        .notNull()
+        //the reference basically is a connection to users schema
+        //which will delete the user if the account is deleted 
+        .references(() => users.id, { onDelete: "cascade" }),
+      type: text("type").$type<AdapterAccount>().notNull(),
+      provider: text("provider").notNull(),
+      providerAccountId: text("providerAccountId").notNull(),
+      refresh_token: text("refresh_token"),
+      access_token: text("access_token"),
+      expires_at: integer("expires_at"),
+      token_type: text("token_type"),
+      scope: text("scope"),
+      id_token: text("id_token"),
+      session_state: text("session_state"),
+    },
+    (account) => ({
+      compoundKey: primaryKey({
+        columns: [account.provider, account.providerAccountId],
+      }),
+    })
+  )

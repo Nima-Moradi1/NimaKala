ee5587d5b222ba02f5fd38650eb20dcac4ac1187
nima moradi <mnima8100@gmail.com>
2024-08-14 00:02:29 +0330

    minor bug fix

--- lib/base-url.ts
+++ lib/base-url.ts
@@ -5,5 +5,5 @@ export default function getBaseURL() {
     if(typeof window !== 'undefined') return ''
     //because vercel intentionally returns "vercel_url" we should set it to our own cutstom domain
     if(process.env.VERCEL_URl) return `https://${process.env.DOMAIN_URL}`
-    'http://localhost:3000'
+   return 'http://localhost:3000'
 }

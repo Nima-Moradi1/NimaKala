9dcb1aaaa038d8a9b349c288f38d9f92c03f3ec5
nima moradi <mnima8100@gmail.com>
2024-08-13 13:49:34 +0330

    ui changes and new new-verification page created

--- components/auth/form-error.tsx
+++ components/auth/form-error.tsx
@@ -6,7 +6,7 @@ export const FormError = ({message} : {message? : string}) => {
     if(!message) return null 
 
     return (
-        <div className="bg-destructive text-secondary-foreground p-3 rounded-xl">
+        <div className="bg-destructive/25 my-2 text-xs flex items-center gap-3 text-secondary-foreground p-3 rounded-xl">
             <AlertCircle className="size-4"/>
             <p>{message}</p>
         </div>
--- components/auth/form-success.tsx
+++ components/auth/form-success.tsx
@@ -2,11 +2,11 @@ import { CheckCircle2 } from "lucide-react"
 
 
 
-export const FormError = ({message} : {message? : string}) => {
+export const FormSuccess = ({message} : {message? : string}) => {
     if(!message) return null 
 
     return (
-        <div className="bg-teal-400 text-secondary-foreground p-3 rounded-xl">
+        <div className="bg-teal-400/25 my-2 text-xs flex items-center gap-3 text-secondary-foreground p-3 rounded-xl">
             <CheckCircle2 className="size-4"/>
             <p>{message}</p>
         </div>

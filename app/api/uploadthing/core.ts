e14edd15ef2d2c6453a30f10323cc1bcf76581d8
nima moradi <mnima8100@gmail.com>
2024-09-04 10:34:37 +0330

    uploadthing configs

---
+++ app/api/uploadthing/core.ts
@@ -0,0 +1,25 @@
+import { metadata } from "@/app/layout"
+import { createUploadthing, type FileRouter } from "uploadthing/next"
+import { UploadThingError } from "uploadthing/server"
+
+const f = createUploadthing()
+
+// FileRouter for your app, can contain multiple FileRoutes
+export const ourFileRouter = {
+  // Define as many FileRoutes as you like, each with a unique routeSlug
+  avatarUploader: f({ image: { maxFileSize: "2MB" } }).onUploadComplete(
+    async ({ metadata, file }) => {}
+  ),
+
+  variantUploader: f({
+    image: { maxFileCount: 10, maxFileSize: "4MB" },
+  })
+    .onUploadError(async ({ error }) => {
+      console.log("error uploading the avatar",error)
+    })
+    .onUploadComplete(async ({ metadata, file }) => {
+      console.log("file uploaded : ",file)
+    }),
+} satisfies FileRouter
+
+export type OurFileRouter = typeof ourFileRouter

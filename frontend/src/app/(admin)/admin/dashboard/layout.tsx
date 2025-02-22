ad5fba8855a7a7c9dae4f5b68337b77546ca7c62
nima moradi <mnima8100@gmail.com>
2025-02-22 11:43:09 +0330

    admin dashboard layout and home page

---
+++ frontend/src/app/(admin)/admin/dashboard/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from "next";
+import "@/app/globals.css";
+import DashboardSidebar from "@/components/dashboard/DashboardSidebar";
+import DashboardHeader from "@/components/dashboard/DashboardHeader";
+
+export const metadata: Metadata = {
+  title: "صفحه پروفایل",
+  description: "پروفایل کاربر",
+}; 
+
+export default function Layout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="">
+    <div className="grid grid-cols-12 h-screen overflow-y-hidden">
+        <aside className="col-span-12 p-3 lg:col-span-3 xl:col-span-2 hidden lg:block">
+            <DashboardSidebar />
+        </aside>
+        <div className="col-span-12 lg:col-span-9 xl:col-span-10 h-screen flex flex-col">
+            <DashboardHeader />
+            <main className="shadow-inner shadow-secondary-300 mr-1 mt-2 rounded-tr-3xl p-4 md:p-6 lg:p-10 flex-1 overflow-y-auto">
+                <div className="xl:max-w-screen-xl">
+                    {children}
+                </div>
+            </main>
+        </div>
+    </div>
+</div>
+  );
+}
---
+++ frontend/src/app/(admin)/admin/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='m-10'>
+admin
+    </div>
+  )
+}
+
+export default page
